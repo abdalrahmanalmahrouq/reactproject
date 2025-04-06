bdd9c86358ac2a4165b182b701389306c39cc965
abood <abdalrahmanadnan209@gmail.com>
2025-04-07 00:44:17 +0300

    adding scrolling feature on the navigation bar

--- src/components/Topnavigation/Topnavigation.jsx
+++ src/components/Topnavigation/Topnavigation.jsx
@@ -1,31 +1,56 @@
 import React, { Component, Fragment } from 'react'
 import { Navbar,Nav,NavDropdown ,Container} from 'react-bootstrap'
-
+import '../../assets/css/custom.css'
+import '../../assets/css/bootstrap.min.css'
  class Topnavigation extends Component {
+  
+    constructor() {
+    super();
+    this.state = {
+      navBarTitle: "Topnavtitle",
+    };
+  }
+
+
+  onScroll = () => {
+    if (window.scrollY > 100) {
+      this.setState({ navBarTitle: "Topnavtitlescroll" });
+    } else {
+      this.setState({ navBarTitle: "Topnavtitle" });
+    }
+  }
+
+    componentDidMount() {
+        window.addEventListener("scroll", this.onScroll);
+        }
+
+
+
   render() {
+
+
     return (
       <Fragment>
-        
-        <Navbar expand="lg" className="bg-body-tertiary" fixed="top" style={{backgroundColor: '#f8f9fa'}}>
-      <Container fluid>
-        <Navbar.Brand href="#">MAHROUQ SERVICES</Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll">
-          <Nav className="me-auto my-2 my-lg-0" style={{ maxHeight: '100px' }}navbarScroll >
-           
-            
-          </Nav>
-          <Nav>
-            <Nav.Link href="#" className="me-2">Login</Nav.Link>
-            <Nav.Link href="#" className="me-2">Register</Nav.Link>
-            <Nav.Link href="#" className="me-2">Home</Nav.Link>
-            <Nav.Link href="#" className="me-2">Services</Nav.Link>      
-            <Nav.Link href="#" className="me-2">Contact</Nav.Link>
-            <Nav.Link href="#" className="me-2">About</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+        <Navbar expand="lg" className="bg-body-tertiary" fixed="top">
+          <Container fluid>
+            <Navbar.Brand href="#" className={this.state.navBarTitle} >
+              MAHROUQ SERVICES
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="navbarScroll" />
+            <Navbar.Collapse id="navbarScroll">
+              <Nav className="me-auto my-2 my-lg-0" style={{ maxHeight: '100px' }} navbarScroll>
+              </Nav>
+              <Nav>
+                <Nav.Link href="#" className="me-2">Login</Nav.Link>
+                <Nav.Link href="#" className="me-2">Register</Nav.Link>
+                <Nav.Link href="#" className="me-2">Home</Nav.Link>
+                <Nav.Link href="#" className="me-2">Services</Nav.Link>
+                <Nav.Link href="#" className="me-2">Contact</Nav.Link>
+                <Nav.Link href="#" className="me-2">About</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
       </Fragment>
     )
   }
